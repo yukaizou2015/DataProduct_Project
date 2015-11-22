a56fdbd97dde94b130e15580151f5db9b391a8ac
YukaiZou <yukaizou2012@gmail.com>
2015-11-22 13:06:31 -0800

    changed to mtcars dataset, add plotting and predicting functions

--- server.R
+++ server.R
@@ -1,18 +1,32 @@
 library(shiny)
+data(mtcars)
+
 shinyServer(
         function(input, output) {
-                output$list1 <- renderPrint({input$list1})
-                output$text2 <- renderPrint({
-                        if(input$list1 == "haireye$Hair") tapply(haireye$Freq, haireye$Hair, sum)
-                        else if(input$list1 == "haireye$Eye") tapply(haireye$Freq, haireye$Eye, sum)
-                                else if(input$list1 == "haireye$Sex") tapply(haireye$Freq, haireye$Sex, sum)
-                                
-                })
+                output$list1 <- renderPrint({c(input$list1)})
+                output$text1 <- renderPrint(
+                        if(input$list1 == "mtcars$cyl") summary(lm(mpg ~ factor(cyl) - 1, data = mtcars))$coef
+                        else if(input$list1 == "mtcars$wt") summary(lm(mpg ~ wt, data = mtcars))$coef
+                        else if(input$list1 == "mtcars$am") summary(lm(mpg ~ factor(am) - 1, data = mtcars))$coef
+                        else if(input$list1 == "mtcars$gear") summary(lm(mpg ~ factor(gear) - 1, data = mtcars))$coef
+                        else if(input$list1 == "mtcars$carb") summary(lm(mpg ~ factor(carb) - 1, data = mtcars))$coef
+                        )
+                        output$text2 <- renderText({paste("The predicted output is:", round(lm(mpg ~ wt, data = mtcars)$coef[1] + lm(mpg ~ wt, data = mtcars)$coef[2] * as.numeric(input$slide),2), "mpg. Check out the plot of mpg against weight.", sep = " ")
+                            })
                 output$Plot <- renderPlot({
-                        haireye <- as.data.frame(HairEyeColor)
-                        if(input$list1 == "haireye$Hair") barplot(tapply(haireye$Freq, haireye$Hair, sum))
-                        else if (input$list1 == "haireye$Eye") barplot(tapply(haireye$Freq, haireye$Eye, sum))
-                        else if (input$list1 == "haireye$Sex") barplot(tapply(haireye$Freq, haireye$Sex, sum))
+                        if(input$list1 == "mtcars$cyl") boxplot(mpg ~ cyl, mtcars, col = "gray", xlab = "Number of cylinders", ylab = "Miles per gallon")
+                        else if (input$list1 == "mtcars$wt") {
+                                plot(y = mtcars$mpg, x = mtcars$wt, pch = 20, xlab = "weight (lb/1000)", ylab = "Miles per gallon")
+                                abline(lm(mpg ~ wt, data = mtcars)$coef[1], lm(mpg ~ wt, data = mtcars)$coef[2], col = "red")
+                                abline(v = input$slide, col = "blue", lwd = 2)
+                                points(x = as.numeric(input$slide), 
+                                       y = lm(mpg ~ wt, data = mtcars)$coef[1] + lm(mpg ~ wt, data = mtcars)$coef[2] * as.numeric(input$slide), pch = 19)
+                        }
+                        else if (input$list1 == "mtcars$am") {
+                                boxplot(mpg ~ am, mtcars, col = "yellow", xlab = "Modes of transmission", ylab = "Miles per gallon", xaxt = "n")
+                                axis(1, at = c(1,2), labels = c("automatic", "manual"))
+                        } 
+                        else if (input$list1 == "mtcars$gear") boxplot(mpg ~ gear, mtcars, col = "lightgreen")
+                        else if (input$list1 == "mtcars$carb") boxplot(mpg ~ carb, mtcars, col = "lightblue")
                 })
-        }
-        )
+})
--- ui.R
+++ ui.R
@@ -1,16 +1,24 @@
 library(shiny)
 shinyUI(pageWithSidebar(
-        headerPanel("Plotting HairEyeColor Dataset"),
-        sidebarPanel(selectInput("list1", "Variable",
-                                 c("Hair" = "haireye$Hair",
-                                   "Eye" = "haireye$Eye",
-                                   "Sex" = "haireye$Sex"
+        headerPanel("Exploring Cars' Performance by Different Characteristics"),
+        sidebarPanel(selectInput("list1", "Select a parameter:",
+                                 c("Weight" = "mtcars$wt",
+                                   "Number of cylinders" = "mtcars$cyl",
+                                   "Transmission" = "mtcars$am",
+                                   "Number of forward gears" = "mtcars$gear",
+                                   "Number of carburetors" = "mtcars$carb"
                                    )
                                  ),
+                     sliderInput('slide', 'Select a weight value for prediction:',
+                                 min = min(mtcars$wt), max = max(mtcars$wt), value = 2, ticks = F),
                      submitButton('Submit')
                      ),
-        mainPanel(verbatimTextOutput('list1'),
-                  verbatimTextOutput('text2'),
+        mainPanel(h2('You selected:'),
+                  verbatimTextOutput('list1'),
+                  h2('Coefficients:'),
+                  verbatimTextOutput('text1'),
+                  textOutput('text2'),
+                  h2('Plot:'),
                   plotOutput('Plot'))
 )
 )
